d05b791bd4c6cf8b97ac25d55e718a627d84deab
no-spark <nostorm@qq.com>
2019-12-05 22:02:30 +0800

    简单的方法

---
+++ MethodTest.java
@@ -0,0 +1,9 @@
+public class MethodTest{
+    public static void main(String[]args){
+        methodDemo();
+    }
+
+    public static void methodDemo(){
+        System.out.println("Hello Shiyanlou");
+    }
+}
