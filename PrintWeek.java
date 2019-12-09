07d85a7666562a73da5f8c0f2369522a1ee9c045
no-spark <nostorm@qq.com>
2019-12-10 00:13:20 +0800

    导入包

---
+++ PrintWeek.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class PrintWeek{
+    public static void main(String[]args){
+        Scanner in=new Scanner(System.in);
+        int x=in.nextInt();
+        switch(x){
+            case 1:
+                System.out.println("今天是星期一");
+                break;
+            case 2:
+                System.out.println("今天是星期二");
+                break;
+            case 3:
+                System.out.println("今天是星期三");
+                break;
+            case 4:
+                System.out.println("今天是星期四");
+                break;
+            case 5:
+                System.out.println("今天是星期五");
+                break;
+            case 6:
+                System.out.println("今天是星期六");
+                break;
+            case 7:
+                System.out.println("今天是星期七");
+                break;
+        }
+
+    }
+}
