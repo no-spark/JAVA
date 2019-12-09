3bd021b6153116643cb036a1d6dedfc986db5c8f
no-spark <nostorm@qq.com>
2019-12-10 00:19:51 +0800

    数组

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+    public static void main(String[]args){
+        int[]a1={1,2,3};
+        int[]a2;
+        a2=a1;
+        for(int i=0;i<a2.length;i++){
+            a2[i]++;
+            System.out.println(a2[i]);
+        }
+    }
+}
