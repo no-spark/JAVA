e3867ed9415d4e1e7d5f2a8ee5bdad5b5e859d52
no-spark <nostorm@qq.com>
2019-12-11 22:13:58 +0800

    构造方法

---
+++ People02.java
@@ -0,0 +1,19 @@
+public class People02{
+    double height;
+    int age;
+    int sex;
+
+    public People02(double h,int a,int s){
+        height=h;
+        age=a;
+        sex=s;
+    }
+
+    public People2(double h,int a){
+        height=h;
+        age=a;
+    }
+
+    People02 li=new People02(168,21,1);
+    People02 lin=new People02(170,26);
+}
